ae3cca78ee267184aabd1e649c357b78c77c9299
UbuntuLKB User <xinyiji96@icloud.com>
2018-02-17 00:47:09 -0800

    m

---
+++ bod-0215/tsdb/skeletons/Index.lisp
@@ -0,0 +1,32 @@
+;;;
+;;; this file should be `Index.lisp' and reside in the directory containing the
+;;; tsdb(1) test suite skeleton databases (typically a subdirectory `skeletons'
+;;; in the tsdb(1) database root directory `*tsdb-home*').
+;;;
+;;; the file should contain a single un-quote()d Common-Lisp list enumerating
+;;; the available skeletons, e.g.
+;;;
+;;;   (((:path . "english") (:content . "English TSNLP test suite"))
+;;;    ((:path . "csli") (:content . "CSLI (ERGO) test suite"))
+;;;    ((:path . "vm") (:content . "English VerbMobil data")))
+;;;
+;;; where the individual entries are assoc() lists with at least two elements:
+;;;
+;;;   - :path --- the (relative) directory name containing the skeleton;
+;;;   - :content --- a descriptive comment.
+;;;
+;;; the order of entries is irrelevant as the `tsdb :skeletons' command sorts
+;;; the list lexicographically before output.
+;;;
+
+(
+((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
+((:path . "lab2") (:content . "lab2: Ling 567 Lab2 assignment"))
+((:path . "lab3") (:content . "lab3: Ling 567 Lab3 assignment"))
+((:path . "lab4") (:content . "lab4: Ling 567 Lab4 assignment"))
+((:path . "lab5_baseline") (:content . "lab5: Ling 567 Lab5 baseline run"))
+((:path . "lab5") (:content . "lab5: Ling 567 Lab5 run"))
+((:path . "test_corpus") (:content . "test_corpus: Ling 567 test corpus"))
+; New test suites here. For example:
+; ((:path . "new-test-suite") (:content . "New Test Suite: A description of the new test suite located at the subdirectory new-test-suite."))
+)
